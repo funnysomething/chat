815446b01513a9c4f12ffdea2816dc1a4e144612
funnysomething <randotheperson@gmail.com>
2024-01-16 20:30:08 -0500

    Refactored message handling, added serverside login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,62 @@
+import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
 
 Map<String, List<Message>> messageList = {};
-Map<String, int> NameToID = {
-  "Daniel": 1,
-  "Naia": 2,
-  "Brandon": 3,
-  "Ethan": 4,
-  "Victor": 5,
-  "Kayla": 6,
-  "Adi": 7
+Map<String, String> contacts = {
+  "thealphatwo": "Adi Satheesh",
+  "miniicecream": "Daniel Zhang"
 };
-Map<int, String> IDToName = {
-  1: "Daniel",
-  2: "Naia",
-  3: "Brandon",
-  4: "Ethan",
-  5: "Victor",
-  6: "Kayla",
-  7: "Adi"
-};
-List<String> contacts = [
-  "Daniel",
-  "Naia",
-  "Brandon",
-  "Ethan",
-  "Victor",
-  "Kayla"
-];
-int homeID = 7;
+String username = 'thealphatwo';
+
+final channel = IOWebSocketChannel.connect('ws://192.168.2.126:8764/');
+final messageStream = StreamController<Message>.broadcast();
+
+Future<void> _listenForMessages() async {
+  try {
+    await for (var rawMessage in channel.stream) {
+      print('Received message: $rawMessage');
+      final dynamic decodedMessage = json.decode(rawMessage);
+      if (decodedMessage['type'] == "message"){
+        final String messageText = decodedMessage['content'];
+        final String chatID = decodedMessage['home'].toString();
+        Message message = Message(messageText, chatID, false);
+        addMessage(chatID, message);
+        messageStream.add(message);
+      }
+    }
+  } catch (error) {
+    print("Error receiving message: $error");
+  }
+}
+
+void addMessage(String chatID, Message message){
+  if (messageList[chatID.toString()] == null) {
+    messageList[chatID.toString()] = [message];
+  } else {
+    messageList[chatID.toString()]!.add(message);
+  }
+}
 
 void main() {
+  _listenForMessages();
+  channel.sink.add('{"type": "login", "username": "$username", "password": "1234"}');
   runApp(const MainApp());
 }
 
 class Message {
   final String text;
+  final String sender;
   final bool fromSelf;
 
-  Message(this.text, this.fromSelf);
+  Message(this.text, this.sender, this.fromSelf);
 
   Map<String, dynamic> toJson() {
     return {
       'text': text,
+      'sender': sender,
       'fromSelf': fromSelf,
     };
   }
@@ -57,7 +68,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: readFromFile(),
+      future: placeholder(), //readFromFile(),
       builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return const MaterialApp(
@@ -80,7 +91,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final channel = IOWebSocketChannel.connect('ws://98.237.89.87:91/');
+
 
   @override
   void initState() {
@@ -98,12 +109,8 @@ class _MainScreenState extends State<MainScreen> {
       body: ListView.builder(
         itemCount: contacts.length,
         itemBuilder: (context, index) {
-          final int chatID = NameToID[contacts[index]]!;
-          final Message lastMessage = messageList[chatID.toString()] != null &&
-                  messageList[chatID.toString()]!.isNotEmpty
-              ? messageList[chatID.toString()]!.last
-              : Message("", false);
-          print("LastMessage: ${lastMessage.text}");
+          List<String> contactList = contacts.keys.toList();
+          final String chatID = contactList[index];
           return Card(
             elevation: 2,
             margin: const EdgeInsets.all(5),
@@ -131,22 +138,14 @@ class _MainScreenState extends State<MainScreen> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            contacts[index],
+                            contacts[contacts.keys.toList()[index]]!,
                             style: const TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 18,
                             ),
                           ),
                           const SizedBox(height: 4),
-                          Text(
-                            lastMessage.text,
-                            style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                            ),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
+                          ChatPreview(chatID: chatID),
                         ],
                       ),
                     ),
@@ -169,7 +168,7 @@ class _MainScreenState extends State<MainScreen> {
 
 class ChatScreen extends StatefulWidget {
   final IOWebSocketChannel channel;
-  final int chatID;
+  final String chatID;
 
   const ChatScreen({super.key, required this.channel, required this.chatID});
 
@@ -184,44 +183,17 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
     super.initState();
-    _listenForMessages();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(IDToName[widget.chatID] ?? ''),
+        title: Text(contacts[widget.chatID] ?? ''),
       ),
       body: Column(
         children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: messageList[widget.chatID.toString()]?.length ?? 0,
-              itemBuilder: (context, index) {
-                final bool alignRight =
-                    messageList[widget.chatID.toString()]![index].fromSelf;
-                return Align(
-                  alignment:
-                      alignRight ? Alignment.centerRight : Alignment.centerLeft,
-                  child: Container(
-                    constraints: const BoxConstraints(maxWidth: 190),
-                    child: Card(
-                      color: Colors.blue,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          messageList[widget.chatID.toString()]![index].text,
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          Expanded(child: Chats(chatID: widget.chatID)),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -248,45 +220,22 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
     );
   }
-  Future<void> _listenForMessages() async {
-    try {
-      await for (var rawMessage in widget.channel.stream) {
-        print('Received message: $rawMessage');
-        final dynamic decodedMessage = json.decode(rawMessage);
-        final String messageText = decodedMessage['message'];
-        final String chatID = decodedMessage['homeID'].toString();
-
-        messageList[chatID.toString()] ??= [];
-        messageList[chatID.toString()]!.add(Message(messageText, false));
-        await writeToFile();
-        setState(() {});
-        _MainScreenState().setState(() {});
-      }
-    } catch (error) {
-      print("Error receiving message: $error");
-    }
-  }
 
   Future<void> _sendMessage(chatID) async {
     try {
       print(chatID);
       print(messageList[chatID]);
 
+      Message message = Message(_textController.text, username, true);
       widget.channel.sink.add(
-          '{"dest": $chatID, "message": "${_textController.text}", "homeID": $homeID}');
+          '{"type": "message", "content": "${_textController.text}", "dest": "$chatID", "home": "$username"}');
 
-      if (messageList[chatID.toString()] == null) {
-        messageList[chatID.toString()] = [Message(_textController.text, true)];
-      } else {
-        messageList[chatID.toString()]!
-            .add(Message(_textController.text, true));
-      }
-      await writeToFile();
-
+      addMessage(chatID, message);
+      // await writeToFile();
       _textController.clear();
       FocusScope.of(context).unfocus();
-      setState(() {});
-      _MainScreenState().setState(() {});
+      messageStream.add(message);
+
     } catch (error) {
       print("Error sending message: $error");
     }
@@ -299,7 +248,87 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
-Widget getIcon(int chatID) {
+class ChatPreview extends StatefulWidget {
+  const ChatPreview({super.key, required this.chatID});
+
+  final String chatID;
+  
+  @override
+  State<ChatPreview> createState() => _ChatPreviewState();
+}
+
+class _ChatPreviewState extends State<ChatPreview> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: messageStream.stream, 
+        builder: (context, snapshot) {
+          return Text(
+            messageList[widget.chatID] != null &&
+                messageList[widget.chatID]!.isNotEmpty
+                ? messageList[widget.chatID]!.last.text
+                : "",
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          );
+        },
+    );
+  }
+}
+
+
+
+
+class Chats extends StatefulWidget {
+  const Chats({super.key, required this.chatID});
+
+  final String chatID;
+
+  @override
+  State<Chats> createState() => _ChatsState();
+}
+
+class _ChatsState extends State<Chats> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Message>(
+      stream: messageStream.stream,
+      builder: (context, snapshot) {
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: messageList[widget.chatID.toString()]?.length ?? 0,
+          itemBuilder: (context, index) {
+            final bool alignRight =
+                messageList[widget.chatID.toString()]![index].fromSelf;
+            return Align(
+              alignment:
+              alignRight ? Alignment.centerRight : Alignment.centerLeft,
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 190),
+                child: Card(
+                  color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      messageList[widget.chatID.toString()]![index].text,
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+Widget getIcon(String chatID) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
     child: CircleAvatar(
@@ -316,48 +345,50 @@ Widget getIcon(int chatID) {
   );
 }
 
-Future<void> writeToFile() async {
-  try {
-    final directory = await getApplicationDocumentsDirectory();
+Future<void> placeholder() async {}
 
-    final messageListFile = File('${directory.path}/messagelist.txt');
-    String encodedlist = json.encode(messageList);
-    print(encodedlist);
-    await messageListFile.writeAsString(encodedlist);
-
-    print('Data written to files');
-  } catch (e) {
-    print("Error writing to file: $e");
-  }
-}
-
-Future<void> readFromFile() async {
-  try {
-    final directory = await getApplicationDocumentsDirectory();
-
-    final messageListFile = File('${directory.path}/messagelist.txt');
-    if (await messageListFile.exists()) {
-      final String messageListContent = await messageListFile.readAsString();
-      final Map<String, dynamic> decodedList = json.decode(messageListContent);
-
-      for (var entry in decodedList.keys) {
-        messageList[entry] = [];
-        for (var message in decodedList[entry]) {
-          messageList[entry]
-              ?.add(Message(message['text'], message['fromSelf']));
-        }
-      }
-      print("Messagelist content: $messageListContent");
-    } else {
-      print("Setting message list values to default");
-      messageList["1"] = [];
-      messageList["2"] = [];
-      messageList["3"] = [];
-      messageList["4"] = [];
-      messageList["5"] = [];
-      messageList["6"] = [];
-    }
-  } catch (e) {
-    print("Error reading from file: $e");
-  }
-}
+// Future<void> writeToFile() async {
+//   try {
+//     final directory = await getApplicationDocumentsDirectory();
+//
+//     final messageListFile = File('${directory.path}/messagelist.txt');
+//     String encodedlist = json.encode(messageList);
+//     print(encodedlist);
+//     await messageListFile.writeAsString(encodedlist);
+//
+//     print('Data written to files');
+//   } catch (e) {
+//     print("Error writing to file: $e");
+//   }
+// }
+//
+// Future<void> readFromFile() async {
+//   try {
+//     final directory = await getApplicationDocumentsDirectory();
+//
+//     final messageListFile = File('${directory.path}/messagelist.txt');
+//     if (await messageListFile.exists()) {
+//       final String messageListContent = await messageListFile.readAsString();
+//       final Map<String, dynamic> decodedList = json.decode(messageListContent);
+//
+//       for (var entry in decodedList.keys) {
+//         messageList[entry] = [];
+//         for (var message in decodedList[entry]) {
+//           messageList[entry]
+//               ?.add(Message(message['text'], message['fromSelf']));
+//         }
+//       }
+//       print("Messagelist content: $messageListContent");
+//     } else {
+//       print("Setting message list values to default");
+//       messageList["1"] = [];
+//       messageList["2"] = [];
+//       messageList["3"] = [];
+//       messageList["4"] = [];
+//       messageList["5"] = [];
+//       messageList["6"] = [];
+//     }
+//   } catch (e) {
+//     print("Error reading from file: $e");
+//   }
+// }
